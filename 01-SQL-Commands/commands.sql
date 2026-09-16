542a8e657ae3ef7593574535e9d60ad061aa32ff
ayushkumartech005 <ayushkumartech005@gmail.com>
2026-09-16 18:58:57 +0530

    Update commands.sql

--- 01-SQL-Commands/commands.sql
+++ 01-SQL-Commands/commands.sql
@@ -1,1 +1,28 @@
+-- SQL Commands Practice
+-- Basic SQL commands
 
+-- 1. Create a table
+CREATE TABLE students (
+    id INT,
+    name VARCHAR(50),
+    age INT
+);
+
+-- 2. Insert data
+INSERT INTO students (id, name, age)
+VALUES (1, 'Ayush', 22);
+
+-- 3. Select data
+SELECT * FROM students;
+
+-- 4. Update data
+UPDATE students
+SET age = 23
+WHERE id = 1;
+
+-- 5. Delete data
+DELETE FROM students
+WHERE id = 1;
+
+-- 6. Drop table
+DROP TABLE students;
